ea85bf9338fba6f9261681b11b5b292cdd9031d6
Bruce <thewayofdragon1987@gmail.com>
2020-04-05 19:47:04 +1000

    Initial Commit

---
+++ Destination Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destination Alarm
+//
+//  Created by Bruce Wang on 5/4/20.
+//  Copyright © 2020 Bruce Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
